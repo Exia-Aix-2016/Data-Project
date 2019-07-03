eb0da7db3a7515218ea5f04c5b2d2153e11673dc
VAUBOURG Mandel <mandel.vaubourg@viacesi.fr>
2019-07-03 16:01:30 +0200

    solution_stat
    @Hougo13 l'oppresseur

--- statistics.py
+++ statistics.py
@@ -1,3 +1,16 @@
 
 
-def solution_stat():
+def solution_stat(solution):
+    result = {
+        "execution_time": 0,
+        "distance": 0,
+        "trucks": 0
+    }
+    print("test")
+    result["distance"] = solution["total_distance"]
+    result["execution_time"] = solution["execution_time"]
+
+    for vehicle in solution["vehicles"]:
+        if vehicle["distance"] > 0:
+            result["trucks"] += 1
+    return result
