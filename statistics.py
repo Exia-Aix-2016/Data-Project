82e07f045ef99161dcc2e30d8ccda35438d1ec54
VAUBOURG Mandel <mandel.vaubourg@viacesi.fr>
2019-07-03 16:23:21 +0200

    add config_stats

--- statistics.py
+++ statistics.py
@@ -13,3 +13,22 @@ def solution_stat(solution):
         if vehicle["distance"] > 0:
             result["trucks"] += 1
     return result
+
+
+def config_stat(solution_stats, cities):
+    result = {
+        "cities": 0,
+        "stats": [
+            {
+                "execution_time": 0,
+                "distance": 0,
+                "trucks": 0,
+            }
+        ]
+    }
+    result["cities"] = cities
+
+    for solution_stat in solution_stats:
+        result["stats"].append(solution_stat)
+
+    return result
