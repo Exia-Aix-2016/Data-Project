daab139ebbdaee2f1491342914cc86ab96bdd777
VAUBOURG Mandel <mandel.vaubourg@viacesi.fr>
2019-07-04 13:31:59 +0200

    histogram

--- chart.py
+++ chart.py
@@ -11,6 +11,58 @@ class Chart:
         self.store = store
 
     def showQuality(self):
+        trucks_fleet = 0
+        trucks_used = 0
+        total_distance = 0
+        whole_distance = 0
+        quality = 0
+        data = []
+        for algo, solution in self.store.solutions.items():
+
+            trucks_fleet = int(solution['trucks_fleet'])
+            total_distance = int(solution['total_distance'])
+            executionTime = int(solution['execution_time'])
+            # CALCULE wholedistance & truckused
+            for truck in solution["vehicles"]:
+                if(len(truck["route"]) > 1):
+                    trucks_used += 1
+            whole_distance = 0
+            indexX = 0
+            indexY = 0
+            for i in self.store.data["distance_matrix"]:
+                for j in i:
+                    if indexX > indexY:
+                        whole_distance += j
+                    indexX += 1
+                indexX = 0
+                indexY += 1
+
+            quality = (total_distance / whole_distance) * \
+                (trucks_used / trucks_fleet)
+
+            data.append(
+                [quality, executionTime, trucks_used, total_distance])
+
+            # PRINT HISTO
+        x = [0, 1, 2, 3]
+        gap = .8 / len(data)
+        print(data)
+        i = 0
+        for i, row in enumerate(data):
+
+            color = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+                     for i in range(8)]
+            print(row)
+            X = np.arange(len(row))
+
+            plt.bar(X + i * gap, row,
+                    width=gap,
+                    color=color)
+        pylab.xticks(x, ["quality", "executionTime",
+                         "trucks_used", "total_distance"], rotation=20)
+        plt.show()
+
+    def showQuality2(self):
 
         trucks_fleet = 0
         trucks_used = 0
@@ -103,13 +155,12 @@ class Chart:
         first_quartile = round(np.percentile(y, 25))
         third_quartile = round(np.percentile(y, 75))
 
-
-        plt.legend(title="average: " + str(round(average, 2)) + 
-                    "\nstandard_deviation: " + str(round(standard_deviation, 2)) +
-                    "\nvariance: " + str(round(variance, 2)) + 
-                    "\nmedian: " + str(round(median, 2)) +
-                    "\nfirst_quartile: " + str(round(first_quartile, 2)) + 
-                    "\nthird_quartile: " + str(round(third_quartile, 2)) )
+        plt.legend(title="average: " + str(round(average, 2)) +
+                   "\nstandard_deviation: " + str(round(standard_deviation, 2)) +
+                   "\nvariance: " + str(round(variance, 2)) +
+                   "\nmedian: " + str(round(median, 2)) +
+                   "\nfirst_quartile: " + str(round(first_quartile, 2)) +
+                   "\nthird_quartile: " + str(round(third_quartile, 2)))
 
         plt.show()
 
